30e1112c80410d35794402508f008c635ed5d929
Nirav Kothari <nirav.kothari@internetbrands.com>
2018-12-08 19:48:06 -0800

    change nav-bar menu template

--- public/js/main.js
+++ public/js/main.js
@@ -1,9 +1,3 @@
-/* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
-function myFunction() {
-    var x = document.getElementById("myTopnav");
-    if (x.className === "topnav") {
-        x.className += " responsive";
-    } else {
-        x.className = "topnav";
-    }
-}
+jQuery(document).on('click', '.mega-dropdown', function(e) {
+    e.stopPropagation()
+})
