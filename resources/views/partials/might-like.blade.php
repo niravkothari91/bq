37c0feb760010553bd3e541acf0d2c359d26e044
Nirav Kothari <nirav.kothari@internetbrands.com>
2019-11-09 15:35:01 -0800

    changes to the might also like section

--- resources/views/partials/might-like.blade.php
+++ resources/views/partials/might-like.blade.php
@@ -1,10 +1,11 @@
 <div class="might-like-section">
+    {{--<hr style="background-color: #cc9e38">--}}
     <div class="container">
         <h2>You might also like...</h2>
         <div class="might-like-grid">
             @foreach ($mightAlsoLike as $product)
                 <a href="{{ route('shop.show', $product->slug) }}" class="might-like-product">
-                    <div class="might-like-product-image"><img src="{{ productImage($product->image) }}" alt="product"></div>
+                    <div class="might-like-product-image-remove"><img src="{{ productImage($product->image) }}" alt="product"></div>
                     <div class="float-bottom">
                         <div class="might-like-product-name">{{ $product->name }}</div>
                         <div class="might-like-product-price">{{ $product->presentPrice() }}</div>
