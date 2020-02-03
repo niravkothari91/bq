0d3d83a118ca0100da2a8a92929bfe09d25e098f
Nirav Kothari <nirav.kothari@internetbrands.com>
2020-02-02 17:11:42 -0800

    revamp the header banner

--- resources/views/partials/banner.blade.php
+++ resources/views/partials/banner.blade.php
@@ -1,2 +1,8 @@
 <div class="header-banner">
-    <span class="banner-title">PURCHASING IN BULK?</span>&nbsp;&nbsp;&nbsp;&nbsp;<span class="banner-description">Reach us at &nbsp;&nbsp;<i class="fa fa-angle-right"></i><i class="fa fa-angle-right"></i>&nbsp;&nbsp;<a href="mailto:contactus@barqualified.com">contactus@barqualified.com</a> | <a href="tel:+919773572294">+91 9773572294</a></span></div>
+    <span class="banner-title">PURCHASING IN BULK?</span>&nbsp;&nbsp;&nbsp;&nbsp;
+    <span class="banner-description">Reach us at &nbsp;&nbsp;<i class="fa fa-angle-right"></i><i class="fa fa-angle-right"></i>&nbsp;&nbsp;
+        <a class="banner-button" href="mailto:contactus@barqualified.com" target="_blank"><i class="fa fa-envelope-o"></i>&nbsp;&nbsp;Email</a>&nbsp;
+        <a class="banner-button" href="tel:+919773572294" target="_blank"><i class="fa fa-phone"></i>&nbsp;&nbsp;Call</a>&nbsp;
+        <a class="banner-button" href="https://wa.me/919723050101?text=Hello!%20I%20am%20interested%20in%20one%20of%20your%20products" target="_blank"><i class="fa fa-whatsapp"></i>&nbsp;&nbsp;Chat</a>
+    </span>
+</div>
