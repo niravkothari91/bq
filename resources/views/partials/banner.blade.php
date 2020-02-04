a69e2e767869658047becfdaa072e1006ed14a18
Nirav Kothari <nirav.kothari@internetbrands.com>
2020-02-04 08:02:18 -0800

    fix the phone number on header banner

--- resources/views/partials/banner.blade.php
+++ resources/views/partials/banner.blade.php
@@ -2,7 +2,7 @@
     <span class="banner-title">PURCHASING IN BULK?</span>&nbsp;&nbsp;&nbsp;&nbsp;
     <span class="banner-description">Reach us at &nbsp;&nbsp;<i class="fa fa-angle-right"></i><i class="fa fa-angle-right"></i>&nbsp;&nbsp;
         <a class="banner-button" href="mailto:contactus@barqualified.com" target="_blank"><i class="fa fa-envelope-o"></i>&nbsp;&nbsp;Email</a>&nbsp;
-        <a class="banner-button" href="tel:+919773572294" target="_blank"><i class="fa fa-phone"></i>&nbsp;&nbsp;Call</a>&nbsp;
+        <a class="banner-button" href="tel:+919723050101" target="_blank"><i class="fa fa-phone"></i>&nbsp;&nbsp;Call</a>&nbsp;
         <a class="banner-button" href="https://wa.me/919723050101?text=Hello!%20I%20am%20interested%20in%20one%20of%20your%20products" target="_blank"><i class="fa fa-whatsapp"></i>&nbsp;&nbsp;Chat</a>
     </span>
 </div>
