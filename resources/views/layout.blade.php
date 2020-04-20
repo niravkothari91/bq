8237ca6cfe3ae5e54bab23b2c8512d49d7bacb4d
Shreyansh Panchal <sunpan835@gmail.com>
2020-04-20 11:37:04 +0000

    Favicon Update

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -10,7 +10,10 @@
 
     <title>BarQualified - It's right in the name!</title>
 
-    <link href="/img/favicon.ico" rel="SHORTCUT ICON" />
+    <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
+    <link rel="manifest" href="/site.webmanifest">
 
     <!-- Fonts -->
     <link href="https://fonts.googleapis.com/css?family=Montserrat%7CRoboto:300,400,700" rel="stylesheet">
